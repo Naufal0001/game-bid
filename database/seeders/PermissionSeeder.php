77941934aa7c24ea211a3b101c86ccb9f6b62bcf
Naufal0001 <zenfx321@gmail.com>
2026-01-18 16:33:55 +0700

    feat: PermissionSeeder

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\PermissionRegistrar;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Reset cached roles & permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        /**
+         * PERMISSIONS
+         */
+        $userPermissions = [
+            'bid auction',
+            'buyout auction',
+            'view transaction',
+        ];
+
+        $sellerPermissions = [
+            'create item',
+            'create auction',
+            'edit own auction',
+            'delete own auction',
+        ];
+
+        $adminPermissions = [
+            'manage auction',
+            'manage item',
+            'manage user',
+            'verify seller',
+            'view reports',
+        ];
+
+        // Create all permissions
+        foreach (array_merge(
+            $userPermissions,
+            $sellerPermissions,
+            $adminPermissions
+        ) as $permission) {
+            Permission::firstOrCreate(['name' => $permission]);
+        }
+
+        /**
+         * ROLES
+         */
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+        $userRole  = Role::firstOrCreate(['name' => 'user']);
+
+        /**
+         * ASSIGN PERMISSIONS
+         */
+        // Admin gets all permissions
+        $adminRole->syncPermissions(Permission::all());
+
+        // User gets default permissions only
+        $userRole->syncPermissions($userPermissions);
+    }
+}
